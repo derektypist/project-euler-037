3484ef8262e6992bedb3959bf07a59281a84a433
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-08-14 14:25:02 +0000

    Added getNumberInfo function at script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,23 @@ function isPrime(num) {
     }
     return PRIMES[num] = true;
 }
+
+// Function to Get Number Information (including Invalid Input)
+function getNumberInfo() {
+    // Set Up Variable
+    let txt = "";
+    // Get the Value of the input field
+    let num = document.getElementById("mynumber").value;
+    // Check if input is valid
+    if (isNaN(num) || num.toString().length == 0 || num<8 || num>11 || !Number.isInteger(Number(num))) {
+        txt += `Invalid Input.  Please enter a whole number between 8 and 11.`
+    } else {
+        // Remove any leading zeros
+        num = parseInt(num);
+        txt += `You have requested ${num} primes. <p>`;
+        txt += `Sum of ${num} primes that are both truncatable from left to right and from right to left is ${truncatablePrimes(num)}.`;
+    }
+
+    // Display Information in the Browser
+    document.getElementById("numinfo").innerHTML = txt;
+}
